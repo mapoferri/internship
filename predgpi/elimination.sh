3713216a1f29aa4394125dfe18542521ae39a9ed
mapoferri <45259338+mapoferri@users.noreply.github.com>
2021-01-22 13:53:11 +0100

    Update elimination.sh

--- predgpi/elimination.sh
+++ predgpi/elimination.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #after the rotation, the files are in this directory
-for file in `ls ./volume/input_fasta/*.fasta`
+for file in `ls ./volume/input/*.fasta`
 do 
 	count=$(grep -Fo '>' $file | wc -l)
 	if [[ $count -gt 1 ]] 
