5d0ba95f66d02d00a91113bb88089be5ddfb1c20
mapoferri <45259338+mapoferri@users.noreply.github.com>
2021-02-02 12:49:07 +0100

    Update psi_blast.sh

--- bacello/pre-process/psi_blast.sh
+++ bacello/pre-process/psi_blast.sh
@@ -1,3 +1,3 @@
 echo `ls ./*.fasta`
 #mkdir ./volume/output/
-for file in `ls -1 ./volume/input_fasta/*.fasta` ; do psiblast -query $file -db ./database/*.fasta -evalue 0.01 -num_iterations 2 -out_ascii_pssm "${file%.fasta}.pssm" -num_alignments 50 -out "${file%.fasta}.alns.blast" ; done                
+for file in `ls -1 ./volume/input_fasta/*_singleline.fasta` ; do psiblast -query $file -db ./database/*.fasta -evalue 0.01 -num_iterations 2 -out_ascii_pssm "${file%.fasta}.pssm" -num_alignments 50 -out "${file%.fasta}.alns.blast" ; done                
